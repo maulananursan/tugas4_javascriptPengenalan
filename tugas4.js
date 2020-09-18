4203966b876c42e351b669faffaf546312316e8d
Maulana Nursan <71365575+maulananursan@users.noreply.github.com>
2020-09-18 11:26:12 +0700

    Add files via upload

---
+++ tugas4.js
@@ -0,0 +1,12 @@
+
+let siswa1 = 200;
+let siswa2 = 180;
+let siswa3 = 170;
+
+if (siswa1 > siswa2) {
+  console.log("tinggi badan Siswa 1 adalah yang tertinggi");
+} else if (siswa2 > siswa3) {
+  console.log("tinggi badan Siswa 2 tertinggi kedua");
+} else if (siswa3 < siswa2) {
+  console.log("tinggi badan Siswa 3 adalah yang terendah");
+}
